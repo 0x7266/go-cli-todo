cf2789a35279f7e23943aba9b1919a5fc05b500c
rafael <rrrr0ffff@gmail.com>
2024-07-31 01:22:49 -0300

    listing and creating todos

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand/v2"
 	"os"
 	"strings"
 )
@@ -13,8 +14,15 @@ type Todo struct {
 	IsDone      bool   `json:"is_done"`
 }
 
+func checkIfDone(status bool) string {
+	if status {
+		return "DONE"
+	}
+	return "NOT DONE"
+}
+
 func main() {
-	// Todos := make([]Todo, 0)
+	Todos := make([]Todo, 0)
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println("What do you want to do next?\nlist\ncreate\nupdate\ndelete")
 
@@ -22,8 +30,16 @@ func main() {
 		line := strings.Split(scanner.Text(), " ")
 		switch line[0] {
 		case "list":
+			for i := 0; i < len(Todos); i++ {
+				fmt.Printf("%v\nID: %v\nStatus: %v\n\n", Todos[i].Description, Todos[i].Id, checkIfDone(Todos[i].IsDone))
+			}
 			break
 		case "create":
+			Todos = append(Todos, Todo{
+				Id:          rand.IntN(100),
+				Description: "todooooooo",
+				IsDone:      false,
+			})
 			break
 		case "update":
 			break
